715c0b9d6f4ed6bbc3014c5da160f5c044ad3ca0
criseri23 <misselb624@gmail.com>
2026-06-18 20:22:59 -0300

    Refactor Gato class and add RealizarExamen method
    Updated Gato class to include RealizarExamen method.

--- MULTIPET/Gato.cs
+++ MULTIPET/Gato.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace MultiPet
+namespace ConsoleApp9
 {
-    public class Gato : Mascota
-    {
-        public override void HacerSonido()
-        {
-            Console.WriteLine("Miau Miau");
-        }
-    }
+ public class Gato : Mascota
+{
+public override void RealizarExamen()
+ {
+ Console.WriteLine($"[Felino] Revisando reflejos y pelaje de {Nombre}");
+ }
+ }
 }
